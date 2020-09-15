436f4c467c8a182ca106209a176520fac73cf3b8
glenn <glenn.jo@kakaocorp.com>
2020-09-15 21:37:45 +0900

    first commit

---
+++ src/main/java/com/sample/springasyncsamplewebflux/SpringAsyncSampleWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.sample.springasyncsamplewebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAsyncSampleWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAsyncSampleWebfluxApplication.class, args);
+    }
+
+}
